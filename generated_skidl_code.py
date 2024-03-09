2d06b647a8bd375f8fc09086f480316d21b35dab
Ohad Forman <ohadforman@mail.tau.ac.il>
2024-03-09 17:58:36 +0200

    Flask work bot make

--- generated_skidl_code.py
+++ generated_skidl_code.py
@@ -1,16 +1,23 @@
 from skidl import *
 
 # Define the components
-leds = [Part('Device', 'LED', ref='LED'+str(i)) for i in range(3)]
-music_player = Part('Device', 'SoundPlayer')
-motion_sensor = Part('Device', 'MotionSensor')
-time_sensor = Part('Device', 'TimeSensor')
+led = Part('device', 'LED', footprint='LED_SMD')
+buzzer = Part('device', 'Buzzer', footprint='BUZZER_SMD')
+fire_sensor = Part('device', 'FIRE_SENSOR', footprint='SENSOR_SMD')
 
-# Define the conditions
-if time_sensor.time > 18:
-    for led in leds:
-        led += VCC
-    music_player.play_music()
-else:
-    for led in leds:
-        led += GND
+# Create the circuit
+led[1, 'A'] += Vcc
+buzzer[1] += led[2, 'K']
+fire_sensor[1, 'G'] += Net('GND')
+
+# Define the power and ground nets
+Vcc = Net('Vcc')
+GND = Net('GND')
+Vcc += Part('power', 'VCC')
+GND += Part('power', 'GND')
+
+# Print the circuit
+generate_netlist()
+
+
+###
